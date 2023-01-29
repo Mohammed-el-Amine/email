fd2f721f4150d4db5d8a83cc31281d30cbb5c400
Ubuntu <ubuntu@vps-f9c60122.vps.ovh.net>
2023-01-29 16:16:36 +0000

    [EDIT] send html mail

--- server/server.py
+++ server/server.py
@@ -1,13 +1,16 @@
 import asyncio
 import smtplib
 import email
+import quopri
 from email.message import EmailMessage
 from aiosmtpd.controller import Controller
 
 class MySMTPHandler:
     async def handle_DATA(self, server, envelope):
         message = EmailMessage()
-        message.set_content(envelope.original_content.decode().encode('ascii', 'ignore').decode().split("MIME-Version: 1.0")[1])
+        decoded_content = quopri.decodestring(envelope.original_content.decode().split("MIME-Version: 1.0")[1]).decode()
+        message.set_content(decoded_content, subtype="html")
+        message.set_charset("utf-8")
         message['From'] = envelope.mail_from
         message['To'] = ', '.join(envelope.rcpt_tos)
         message['Subject'] = envelope.original_content.decode().split("Subject: ")[1].split("\n")[0]
@@ -15,7 +18,6 @@ class MySMTPHandler:
         smtp_server.send_message(message)
         smtp_server.quit()
         return '250 OK'
-
 
 controller = Controller(MySMTPHandler, hostname='146.59.14.191', port=8025)
 loop = asyncio.get_event_loop()
